284f6ec93f3a8ae91e351fc64ec83dc372929d36
Haisem <haisem.jemal@gmail.com>
2019-10-18 11:39:16 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component{
+
+    render(){
+        return(
+            <div className="ui container">
+                App
+            </div>
+        )
+    }
+}
+
+export default App;
